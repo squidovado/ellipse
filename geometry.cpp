96394999a6ccb0c752c66774c2dbb5ae24254f47
Alex <metallexey@mail.ru>
2021-02-19 17:55:46 +0300

    two points calculation fixed

--- geometry.cpp
+++ geometry.cpp
@@ -3,14 +3,14 @@
 #include "geometry.h"
 #include "common.h"
 
-double* solve3(const double (&matrix)[3][3], const double (&rhs)[3])
+bool solve3(const double (&matrix)[3][3], const double (&rhs)[3], double (&solution)[3])
 {
     //Cramer's rule
     double det = matrix[0][0]*matrix[1][1]*matrix[2][2]+matrix[0][1]*matrix[1][2]*matrix[2][0]+
             matrix[0][2]*matrix[1][0]*matrix[2][1]-matrix[0][0]*matrix[1][2]*matrix[2][1]-
             matrix[0][1]*matrix[1][0]*matrix[2][2]-matrix[0][2]*matrix[1][1]*matrix[2][0];
     if (isEqual(det,0.0))
-        return nullptr;
+        return false;
     double det1 = rhs[0]*matrix[1][1]*matrix[2][2]+matrix[0][1]*matrix[1][2]*rhs[2]+
             matrix[0][2]*rhs[1]*matrix[2][1]-rhs[0]*matrix[1][2]*matrix[2][1]-
             matrix[0][1]*rhs[1]*matrix[2][2]-matrix[0][2]*matrix[1][1]*rhs[2];
@@ -20,16 +20,30 @@ double* solve3(const double (&matrix)[3][3], const double (&rhs)[3])
     double det3 = matrix[0][0]*matrix[1][1]*rhs[2]+matrix[0][1]*rhs[1]*matrix[2][0]+
             rhs[0]*matrix[1][0]*matrix[2][1]-matrix[0][0]*rhs[1]*matrix[2][1]-
             matrix[0][1]*matrix[1][0]*rhs[2]-rhs[0]*matrix[1][1]*matrix[2][0];
-    double* solution = new double[3];
     solution[0]=det1/det;
     solution[1]=det2/det;
     solution[2]=det3/det;
-    return solution;
+    return true;
+}
+
+void Line::set(double a, double b, double c, double d)
+{
+    _a = a;
+    _b = b;
+    _c = c;
+    _d = d;
 }
 
 Line Line::translated(QPointF point) const
 {
-    return Line(); //Line(_a-point.x(),_b,_c-point.y(),_d);
+    return Line(_a-point.x(),_b,_c-point.y(),_d);
+}
+
+void Ellipse::setParameters(double a11, double a12, double a22)
+{
+    _a11 = a11;
+    _a12 = a12;
+    _a22 = a22;
 }
 
 Placer::Placer(QObject *parent) : QObject(parent), line1(), line2(),
@@ -40,66 +54,84 @@ Placer::Placer(QObject *parent) : QObject(parent), line1(), line2(),
 
 void Placer::processNewData(QSharedPointer<const InputData> data)
 {
-    checkData(data);
+    if (!checkData(data))
+        return;
+    ellipse.setCenter(QPointF(data->xcenter, data->ycenter));
+    tpoint1 = QPointF(data->point1x, data->point1y);
+    tpoint2 = QPointF(data->point2x, data->point2y);
+    line1.set(tpoint1.x(),data->tangent1b,tpoint1.y(),-data->tangent1a);
+    if (data->mode == InputData::TwoTangentsOnePoint)
+    {
+        double a2,c2;
+        if (isEqual(data->tangent2b,0.0))
+        {
+            a2 = data->tangent2c/data->tangent2a;
+            c2 = 0.0;
+        }
+        else
+        {
+            a2 = 0.0;
+            c2 = data->tangent2c/data->tangent2b;
+        }
+        line2.set(a2,data->tangent2b,c2,-data->tangent2a);
+        calculateTwoLinesOnePoint();
+    }
+    else
+        calculateTwoPointsOneLine();
 }
 
 bool Placer::checkData(QSharedPointer<const InputData> data) const
 {
     if (isEqual(data->tangent1a,0) && isEqual(data->tangent1b,0))
     {
-        QMessageBox::warning(nullptr, "Bad input", "First tangent must have at least one non-zero variable coefficient.");
+        errorMessage(0);
         return false;
     }
     if (data->mode == InputData::TwoTangentsOnePoint && isEqual(data->tangent2a,0) && isEqual(data->tangent2b,0))
     {
-        QMessageBox::warning(nullptr, "Bad input", "Second tangent must have at least one non-zero variable coefficient.");
+        errorMessage(1);
         return false;
     }
     if (isEqual(data->tangent1a*data->xcenter + data->tangent1b*data->ycenter, data->tangent1c))
     {
-        QMessageBox::warning(nullptr, "Bad input", "Ellipse center shouldn't belong to first tangent.");
+        errorMessage(2);
         return false;
     }
     if (data->mode == InputData::TwoTangentsOnePoint && isEqual(data->tangent2a*data->xcenter + data->tangent2b*data->ycenter, data->tangent2c))
     {
-        QMessageBox::warning(nullptr, "Bad input", "Ellipse center shouldn't belong to second tangent.");
-        return false;
-    }
-    if (isEqual(data->xcenter,data->point1x) && isEqual(data->ycenter,data->point1y))
-    {
-        QMessageBox::warning(nullptr, "Bad input", "Ellipse center and first point of tangency shouldn't coincide.");
+        errorMessage(3);
         return false;
     }
     if (data->mode == InputData::OneTangentTwoPoints && isEqual(data->xcenter,data->point2x) && isEqual(data->ycenter,data->point2y))
     {
-        QMessageBox::warning(nullptr, "Bad input", "Ellipse center and second point of tangency shouldn't coincide.");
+        errorMessage(4);
         return false;
     }
     if(!isEqual(data->tangent1a*data->point1x + data->tangent1b*data->point1y, data->tangent1c))
     {
-        QMessageBox::warning(nullptr, "Bad input", "First tangent must pass through first point of tangency.");
+        errorMessage(5);
         return false;
     }
     if(data->mode == InputData::TwoTangentsOnePoint && isEqual(data->tangent2a*data->point1x + data->tangent2b*data->point1y, data->tangent2c))
     {
-        QMessageBox::warning(nullptr, "Bad input", "Second tangent shouldn't pass through first point of tangency.");
+        errorMessage(6);
         return false;
     }
     if(data->mode == InputData::OneTangentTwoPoints && isEqual(data->tangent1a*data->point2x + data->tangent1b*data->point2y, data->tangent1c))
     {
-        QMessageBox::warning(nullptr, "Bad input", "First tangent shouldn't pass through second point of tangency.");
+        errorMessage(7);
         return false;
     }
     return true;
 }
 
-void Placer::calculate()
+void Placer::calculateTwoPointsOneLine()
 {
     //local coord system with the origin at the center of the ellipse
     QPointF origin = ellipse.center();
-    QPointF p1local;// = p1 - origin;
-    QPointF p2local;// = p2 - origin;
-    Line l1local;// = l1.translated(origin);
+    QPointF p1local = tpoint1 - origin;
+    QPointF p2local = tpoint2 - origin;
+    Line l1local = line1.translated(origin);
     double matrix[3][3], rhs[3] = {1., 1., 0.};
     matrix[0][0] = p1local.x()*p1local.x();
     matrix[0][1] = 2*p1local.x()*p1local.y();
@@ -110,5 +142,32 @@ void Placer::calculate()
     matrix[2][0] = p1local.x()*l1local.b();
     matrix[2][1] = p1local.x()*l1local.d()+p1local.y()*l1local.b();
     matrix[2][2] = p1local.y()*l1local.d();
-    double* solution = solve3(matrix, rhs);
+    double solution[3];
+    if (!solve3(matrix, rhs, solution) || solution[0]<=0.0 || solution[2]<=0.0 || solution[0]*solution[2]-solution[1]*solution[1]<=0.0)
+    {
+        errorMessage(8);
+        return;
+    }
+    ellipse.setParameters(solution[0], solution[1], solution[2]);
+    line2.set(tpoint2.x(),solution[1]*p2local.x()+solution[2]*p2local.y(),
+              tpoint2.y(),-solution[0]*p2local.x()-solution[1]*p2local.y());
+}
+
+void Placer::calculateTwoLinesOnePoint()
+{
+
+}
+
+void Placer::errorMessage(quint8 error) const
+{
+    QString errors[9] = {"First tangent must have at least one non-zero variable coefficient.",
+                        "Second tangent must have at least one non-zero variable coefficient.",
+                        "Ellipse center shouldn't belong to first tangent.",
+                        "Ellipse center shouldn't belong to second tangent.",
+                        "Ellipse center and second point of tangency shouldn't coincide.",
+                        "First tangent must pass through first point of tangency.",
+                        "Second tangent shouldn't pass through first point of tangency.",
+                        "First tangent shouldn't pass through second point of tangency.",
+                        "Ellipse solution can't be found (might be hyperbola or imaginary ellipse)."};
+    QMessageBox::warning(nullptr, "Bad input", errors[error]);
 }
--- geometry.h
+++ geometry.h
@@ -10,7 +10,9 @@ class Line
 {
 public:
     Line() : _a(0.0), _b(1.0), _c(0.0), _d(0.0) {}
+    Line(double A, double B, double C, double D) : _a(A), _b(B), _c(C), _d(D) {}
     Line translated(QPointF) const;
+    void set(double, double, double, double);
     double a() const {return _a;}
     double b() const {return _b;}
     double c() const {return _c;}
@@ -24,14 +26,15 @@ private:
 class Ellipse
 {
 public:
-    Ellipse() : a11(1.0), a12(0.0), a22(4.0), _center(1.0,-0.5) {}
+    Ellipse() : _a11(1.0), _a12(0.0), _a22(4.0), _center(1.0,-0.5) {}
     QRectF boundingRect() const;
     void setCenter(QPointF point) {_center = point;}
+    void setParameters(double, double, double);
     QPointF center() const {return _center;}
 private:
     //the ellipse is stored in a coordinate system with the origin in its center
     //so it can be presented as a11*x^2+2*a12*x*y+a22*y^2=1, where a11>0, a22>0, a11*a22-a12^2>0
-    double a11, a12, a22;
+    double _a11, _a12, _a22;
     QPointF _center;
 };
 
@@ -44,7 +47,9 @@ public slots:
     void processNewData(QSharedPointer<const InputData>);
 private:
     bool checkData(QSharedPointer<const InputData>) const;
-    void calculate();
+    void calculateTwoLinesOnePoint();
+    void calculateTwoPointsOneLine();
+    void errorMessage(quint8) const;
 
     Line line1, line2;
     QPointF tpoint1, tpoint2;
